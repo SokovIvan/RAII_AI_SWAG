b0b07fca6690acb4fa06e410f697e52be55c5cdd
insookwa <nsookwaivan@gmail.com>
2025-08-08 18:13:39 +0400

    добавлены индексный файл, загружаемый звуковой файл, , файл заявки и сам файл тикетов. Добавлен URL-путь к индексному файлу и загружаемому звуковому файлу

--- backend/src/backend/interface/views.py
+++ backend/src/backend/interface/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+
+    return render (request, "index.html")
+
+
+def upload(request):
+
+    return render (request, "upload_sound.html")
